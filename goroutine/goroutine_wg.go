f410079825688580fe5be06f00a2dfbc52c948cb
ipochuev <pochuevia@altarix.ru>
2017-09-18 13:18:14 +0300

    some primitives

--- goroutine/goroutine_wg.go
+++ goroutine/goroutine_wg.go
@@ -5,7 +5,7 @@ import (
 	"sync"
 )
 
-func main()  {
+/*func main()  {
 	wg := new(sync.WaitGroup)
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
@@ -15,4 +15,31 @@ func main()  {
 		}(i, wg)
 	}
 	wg.Wait()
+}*/
+
+
+//Next example
+
+var (
+	wg = sync.WaitGroup{}
+	counter = 0
+)
+
+func main() {
+	for i := 0; i < 10; i++ {
+		wg.Add(2)
+		go sayHello()
+		go increment()
+	}
+	wg.Wait()
+}
+
+func sayHello()  {
+	fmt.Printf("Hello #%v\n", counter)
+	wg.Done()
+}
+
+func increment()  {
+	counter++
+	wg.Done()
 }
